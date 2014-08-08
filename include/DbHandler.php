82eb1266ea272c88dbb232e1538ee26b7b5ea5f9
hungmutsu <hungmutsu@gmail.com>
2014-08-08 11:04:46 +0700

    Revert "Revert "update""
    This reverts commit 720a5ad7adb61e5d67d9d78230b987412726b20a.

--- include/DbHandler.php
+++ include/DbHandler.php
@@ -11,7 +11,7 @@ class DbHandler {
 
     private $conn;
 
-    function __construct() {
+    function __construct() {    
         require_once dirname(__FILE__) . '/DbConnect.php';
         // opening db connection
         $db = new DbConnect();
@@ -169,21 +169,20 @@ class DbHandler {
      * @param String $task task text
      */
     public function createTask($user_id, $taskName, $taskDescription,$dueDate, $reminderDate,
-        $isReminder, $isDueDate, $isComplete, $createdDate, $updatedDate) {
-            
-        $dateTime = new DateTime("now");
-        if($createdDate == NULL || $createdDate == "") {
-            $createdDate = $dateTime->format("Y-m-d H:i:s");    
-        }
-        if($updatedDate == NULL)
-        {
-            $updatedDate = $dateTime->format("Y-m-d H:i:s");
+        $isReminder, $isDueDate, $isComplete, $updatedDate) {
+           
+        if($updatedDate == NULL || $updatedDate == ""){
+            $updatedDate = new DateTime("now");
+        } else {
+            $updatedDate = new DateTime($updatedDate);
         }
         
+        $updateFormat = $updatedDate->format("Y-m-d H:i:s");
+        
         $stmt = $this->conn->prepare("INSERT INTO tasks(userId, taskName, taskDescription, dueDate, reminderDate,
-            isReminder, isDueDate, isComplete, createdDate,updatedDate) VALUES(?,?,?,?,?,?,?,?,?,?)");
-        $stmt->bind_param("isssssssss", $user_id, $taskName, $taskDescription,$dueDate, $reminderDate,
-            $isReminder, $isDueDate, $isComplete, $createdDate, $updatedDate);
+            isReminder, isDueDate, isComplete,  updatedDate) VALUES(?,?,?,?,?,?,?,?,?)");
+        $stmt->bind_param("issssssss", $user_id, $taskName, $taskDescription,$dueDate, $reminderDate,
+            $isReminder, $isDueDate, $isComplete, $updateFormat);
         $result = $stmt->execute();
         $stmt->close();
 
@@ -269,24 +268,19 @@ class DbHandler {
     public function updateTask($task_id, $taskName, $taskDescription,$dueDate, $reminderDate, $isReminder,
         $isDueDate, $isComplete, $updatedDate ) {
             
-            $dueDateDefault = new DateTime();
-            $updatedDateDefault = new DateTime();
-            $reminderDateDefault = new DateTime();
-            
-        if($updatedDate != NULL || $updatedDate != ""){
-            date_timestamp_set($updatedDateDefault, $updatedDate);
+           
+        if($updatedDate == NULL || $updatedDate == ""){
+            $updatedDate = new DateTime("now");
+        } else {
+            $updatedDate = new DateTime($updatedDate);
         }
-        if($reminderDate != NULL || $reminderDate != "") {
-         date_timestamp_set($reminderDateDefault, $reminderDate);   
-        }
-        if($dueDate != NULL || $dueDate != "") {
-         date_timestamp_set($dueDateDefault, $dueDate);   
-        }
+        
+        $updateDateFormat = $updatedDate->format("Y-m-d H:i:s");
         
         $stmt = $this->conn->prepare("UPDATE tasks SET taskName = ?, taskDescription = ?,dueDate = ?,
             reminderDate = ?, isReminder = ?, isDueDate = ?, isComplete = ?, updatedDate = ? WHERE taskId = ?");
-        $stmt->bind_param("ssssssssi", $taskName, $taskDescription,$dueDateDefault, $reminderDateDefault,
-            $isReminder, $isDueDate, $isComplete, $updatedDateDefault, $task_id);
+        $stmt->bind_param("ssssssssi", $taskName, $taskDescription,$dueDate, $reminderDate,
+            $isReminder, $isDueDate, $isComplete, $updateDateFormat, $task_id);
         $stmt->execute();
         $num_affected_rows = $stmt->affected_rows;
         $stmt->close();
